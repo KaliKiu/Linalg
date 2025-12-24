4fac156454469aee61d2b290f33f7f797e71c94b
KaliKiu <palablink@gmail.com>
2025-12-24 11:54:02 +0100

    setting up matrix exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,17 @@ int main(){
     try{
     Vector vec({1,2,3});
     printVec(vec);
-    Matrix<double,1,1> mat;
+
+    std::vector<std::vector<double>> vecc = {{1,3,3},{2,4,5}};
+    Matrix<double,0,3> mat;
+    
+    
+
     }catch (const std::bad_alloc& e){
         std::cerr <<"Out of memory: "<<e.what()<<"\n";
         return EXIT_FAILURE;
+    }catch(const MatrixError::InvalidMatrix& e){
+        std::cerr<<"error meow"<<e.what()<<"\n";;
     }catch ( const std::exception& e){
         std::cerr <<"Error "<<e.what() <<"\n";
         return EXIT_FAILURE;
--- src/math/matrix.h
+++ src/math/matrix.h
@@ -3,6 +3,19 @@
 
 #include <stdlib.h>
 #include <vector>
+#include <iostream>
+
+namespace MatrixError{
+    class InvalidMatrix : public std::exception {
+        protected:
+        std::string message;
+        public: 
+        explicit InvalidMatrix(std::string msg) : message(std::move(msg)){};
+        const char* what() const noexcept override{
+            return message.c_str();
+        }
+    };
+}
 template<typename T,std::size_t Rows,std::size_t Cols>
 class Matrix{
     public: 
@@ -10,21 +23,41 @@ class Matrix{
     std::size_t rows;
     std::size_t cols;
 
-    explicit Matrix(){
+    explicit Matrix(): rows{Rows}, cols{Cols}{
+        if(rows == 0){
+            throw MatrixError::InvalidMatrix("Invalid Matrix: ROWS == 0\n");
+        }else if(cols ==0){
+            throw MatrixError::InvalidMatrix("Invalid Matrix: COLS == 0\n");
+        }
         for(std::size_t i = 0; i<Rows; i++){
-            for(std::size_t j = 0; j<Cols;j++){
+            for(std::size_t j = 0; j<Cols; j++){
                 data[i][j]= T{};
             }
         }
     }
     void fill(const std::vector<std::vector<T>>& arr){
         for(std::size_t i = 0;i<rows; i++){
-            for(std::size_t j = 0; j<cols;j++){
+            for(std::size_t j = 0; j<cols; j++){
                 data[i][j] = arr[i][j];
             }
         }
     }
+    void addVector(const Vector vector){
+        for(std::size_t i = 0; i<cols; i++){
+            data[rows+1][i] = vector.components[i];
+        }
+        rows += 1;
+    }
+    void print(){
+        for(std::size_t i = 0; i<rows; i++){
+            for(std::size_t j=0; j<cols; j++){
+                std::cout <<data[i][j] << " ";
+            }
+        std::cout <<"\n";
+        }
+    }
     
 };
+
 
 #endif
